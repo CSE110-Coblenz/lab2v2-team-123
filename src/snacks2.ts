f61923a76477ef31079425f66f057873fbd56b1d
Jason <jwilkens@ucsd.edu>
2025-10-09 11:34:14 -0700

    jason's snacks 2

---
+++ src/snacks2.ts
@@ -0,0 +1,9 @@
+let snacks: string[] = ["chips", "soda", "cookies"];
+
+function printSnacks(x: string[]) {
+    for (let i: number = 0; i < x.length; i++) {
+        console.log(x[i]);
+    }
+}
+
+const message = printSnacks(snacks);
