9b89b55c3ab529f482c7ecd3fa0c603ec7ca086d
Jason <jwilkens@ucsd.edu>
2025-10-09 11:49:45 -0700

    Jasons music 2

---
+++ src/music.ts
@@ -0,0 +1,10 @@
+let music: string[] = ["pop", "rap", "country"];
+
+function printMusic(x: string[]) {
+    for (let i: number = 0; i < x.length; i++) {
+        console.log(x[i]);
+    }
+}
+
+const message2 = printMusic(music);
+//
