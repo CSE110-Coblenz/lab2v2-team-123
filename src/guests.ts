f823f52032f03d8ae26d36b1b471421c26b5e497
Noah-Arechiga <narechiga@ucsd.edu>
2025-10-09 11:35:13 -0700

    Added guest list and function

---
+++ src/guests.ts
@@ -0,0 +1,9 @@
+let guestlist: string[] = ["LeBron James", "Nas", "Shohei Ohtani"]
+
+function printGuests(guests: string[]) {
+    for (let i: number = 0; i < guests.length; i++) {
+        console.log(guests[i]);
+    }
+}
+
+printGuests(guestlist);
